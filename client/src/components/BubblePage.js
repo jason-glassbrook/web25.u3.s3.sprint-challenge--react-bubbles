4b00b8dfb681d61834bd74728d7d47c0c408ebcb
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2019-12-20 13:37:56 -0500

    GET list of colors

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -7,9 +7,23 @@ import { server } from 'routes'
 import { authios } from 'tools/auth'
 
 const BubblePage = () => {
-  const [colorList, setColorList] = React.useState ([])
+  const [ colorList, setColorList ] = React.useState ([])
+
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  React.useEffect (() => {
+    authios ()
+      .get (
+        server.colors.GET ()
+      )
+      .then ((response) => {
+        console.log (response)
+        setColorList (response.data)
+      })
+      .catch ((error) => {
+        console.log (error)
+      })
+  }, [])
 
   return (
     <Fragment>
