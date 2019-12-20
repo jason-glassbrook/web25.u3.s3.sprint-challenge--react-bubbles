0b0ebbdf0f8fa5c4973c4b921be05ba80c5e8366
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2019-12-20 13:16:01 -0500

    create Login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,98 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+import { client, server } from 'routes'
+import { authios, user } from 'tools/auth'
+
+const init = {
+  state : {
+    creds : {
+      username : '',
+      password : '',
+    },
+    isFetching : false,
+  }
+}
+
+const Login = (props) => {
+  const [ state, setState ] = React.useState (init.state)
+
+  const setCred = (name, value) => {
+    setState ((state) => ({
+      ...state,
+      creds : {
+        ...state.creds,
+        [name] : value,
+      },
+    }))
+  }
+
+  const setIsFetching = (isFetching) => {
+    setState ((state) => ({
+      ...state,
+      isFetching,
+    }))
+  }
+
+  const handleCredChange = ({ target : { name, value } }) => {
+    setCred (name, value)
+  }
+
+  const tryLogin = (e) => {
+    e.preventDefault ()
+
+    setIsFetching (true)
+
+    authios ()
+    .post (server.login.POST (), state.creds)
+    .then ((response) => {
+
+      console.log (response)
+
+      setIsFetching (false)
+      user.token.set (response.data.payload)
+      user.isAllowed.set (true)
+
+      props.history.push (client.bubbles ())
+
+    })
+    .catch ((error) => {
+
+      console.log (error)
+
+      setIsFetching (false)
+      user.token.clear ()
+      user.isAllowed.set (false)
+
+    })
+  }
+
   return (
-    <div className='Login'>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+    <div className='Login login-page'>
+
+      <form
+      className='login-form'
+      onSubmit={tryLogin}>
+        <input
+          type='text'
+          name='username'
+          value={state.creds.username}
+          onChange={handleCredChange}
+        />
+        <input
+          type='password'
+          name='password'
+          value={state.creds.password}
+          onChange={handleCredChange}
+        />
+        <button type='submit'>Log In</button>
+      </form>
+
+      {state.isFetching ? (
+        <div>logging in</div>
+      ) : (
+        <Fragment/>
+      )}
+
     </div>
   )
 }
