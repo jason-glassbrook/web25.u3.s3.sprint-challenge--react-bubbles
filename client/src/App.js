fc89fa4769d4e6235b4b8aeaecf574522b6ccec3
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2019-12-20 12:22:28 -0500

    convert to using Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom'
 
 import Login from './components/Login'
 import './styles.scss'
@@ -8,14 +12,21 @@ function App () {
   return (
     <Router>
       <div className='App'>
-        <Route
-        exact path={[ '/', '/login' ]}
-        component={Login}
-        />
-        {/*
-          Build a PrivateRoute component that will
-          display BubblePage when you're authenticated
-        */}
+        <Switch>
+          {/*
+            Build a PrivateRoute component that will
+            display BubblePage when you're authenticated
+          */}
+          {/* <Route exact path='/logout'
+          component={Logout}
+          /> */}
+          <Route exact path='/login'
+          component={Login}
+          />
+          <Route
+          component={Login}
+          />
+        </Switch>
       </div>
     </Router>
   )
