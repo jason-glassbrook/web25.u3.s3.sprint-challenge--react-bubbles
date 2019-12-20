c676619a83b43b48d8b0425752e59479b2f69079
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2019-12-20 12:29:59 -0500

    add blank Logout page

---
+++ client/src/components/Logout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logout = () => {
+  return (
+    <div className='Logout'>
+      <h1>Welcome to the Bubble App!</h1>
+      <p>Build a logout page here</p>
+    </div>
+  )
+}
+
+export default Logout
