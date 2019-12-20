fd326d6c564aa79bd637a82e0d090ddc65af35f4
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2019-12-20 12:56:06 -0500

    fix import of user; add server route

--- client/src/tools/auth/authios.js
+++ client/src/tools/auth/authios.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 import { server } from 'routes'
-import user from 'tools/user'
+import user from './user'
 
 const authios = () => {
   return axios.create ({
-    baseURL : server.base (),
+    baseURL : server.base,
     headers : {
       Authorization : user.token.get (),
     },
