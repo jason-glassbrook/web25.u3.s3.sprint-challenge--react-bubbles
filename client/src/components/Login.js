9890e6ede056f2894084b74c2877e96cec30c115
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2019-12-20 12:28:20 -0500

    give Login a class

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,10 +4,10 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
+    <div className='Login'>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
-    </>
+    </div>
   )
 }
 
